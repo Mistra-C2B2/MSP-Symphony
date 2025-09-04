6d0b763984040efbae1d544807ab70e8f035ba02
luisheres <luis.heres@protonmail.com>
2025-09-04 13:53:53 +0000

    Automatic creation of certificates

--- .devcontainer/scripts/start-frontend.sh
+++ .devcontainer/scripts/start-frontend.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-cd /workspace/frontend && PROXY_TARGET=local ng serve --ssl --disable-host-check --host 0.0.0.0
+set -e
+
+CERT="/workspace/frontend/ssl/server.crt"
+KEY="/workspace/frontend/ssl/server.key"
+
+# Check if certificate and key exist
+if [ ! -f "$CERT" ] || [ ! -f "$KEY" ]; then
+  echo "SSL certificate or key not found. Generating..."
+  npm run generate-cert
+fi
+
+cd /workspace/frontend && PROXY_TARGET=local ng serve --ssl --host 0.0.0.0
