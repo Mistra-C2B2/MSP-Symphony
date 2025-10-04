e59f222750a347a3b1cf18b9275c0a471a185384
Maria Mair <mm225mz@student.lnu.se>
2025-10-04 10:51:06 +0000

    Fix File not found issue and add npm install

--- .devcontainer/scripts/start-frontend.sh
+++ .devcontainer/scripts/start-frontend.sh
@@ -4,10 +4,14 @@ set -e
 CERT="/workspace/frontend/ssl/server.crt"
 KEY="/workspace/frontend/ssl/server.key"
 
+cd /workspace/frontend
+
 # Check if certificate and key exist
 if [ ! -f "$CERT" ] || [ ! -f "$KEY" ]; then
   echo "SSL certificate or key not found. Generating..."
   npm run generate-cert
 fi
 
-cd /workspace/frontend && PROXY_TARGET=local ng serve --ssl --host 0.0.0.0
+npm install
+
+PROXY_TARGET=local ng serve --ssl --host 0.0.0.0
